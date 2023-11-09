5a4c4e63f059c6ed7ec9f7cdaae4e72315205fb4
v187nce <134378283+v187nce@users.noreply.github.com>
2023-11-09 17:30:58 +0000

    hs

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Queue;
+
+public class Main{
+    
+    Stack<Teller> teller = new Stack<Teller>(); 
+    Queue<Biber>  biber = new Queue<Biber>();
+
+    public static void main(String[] args){
+        
+        
+
+        
+    }
+}
