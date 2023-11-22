d9d10769d946bf0b956b83523fb6b8f2c21ee359
v187nce <134378283+v187nce@users.noreply.github.com>
2023-11-22 08:12:24 +0000

    nix

---
+++ InformatikBiber/Biber.java
@@ -0,0 +1,14 @@
+package InformatikBiber; 
+
+public class Biber{
+
+    private int groesse; 
+
+    public Biber(int groesse){
+        this.groesse = groesse; 
+    }
+
+    public int getGroesse(){
+        return groesse; 
+    }
+}
---
+++ InformatikBiber/Teller.java
@@ -0,0 +1,14 @@
+package InformatikBiber;
+
+public class Teller{
+    
+    private int groesse; 
+
+    public Teller(int groesse){
+        this.groesse = groesse; 
+    }
+
+    public int getGroesse(){
+        return groesse; 
+    }
+}
