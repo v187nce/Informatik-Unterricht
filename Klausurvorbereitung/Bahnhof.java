bc01283d4deb6f14a82214768a05c2d67a27c21a
v187nce <134378283+v187nce@users.noreply.github.com>
2023-11-24 11:15:01 +0000

    Klausurvorbereitung

---
+++ Klausurvorbereitung/Bahnhof.java
@@ -0,0 +1,39 @@
+package Klausurvorbereitung; 
+
+public class Bahnhof{
+    private Stack<Wagon> gleisA;  
+    private Stack<Wagon> gleisB;
+    private Stack<Wagon> gleisC;
+
+    public Bahnhof(){
+        gleisA = new Stack<Wagon>();
+        gleisB = new Stack<Wagon>();
+        gleisC = new Stack<Wagon>();
+    }
+
+    public void rangiereAzuB(){
+        gleisB.push(gleisA.top()); 
+        gleisA.pop();
+    }
+    public void rangiereAzuC(){
+        gleisC.push(gleisA.top()); 
+        gleisA.pop();
+    }
+    public void rangiereBzuA(){
+        gleisA.push(gleisB.top()); 
+        gleisB.pop();
+    }
+    public void rangiereBzuC(){
+        gleisC.push(gleisB.top()); 
+        gleisB.pop();
+    }
+    public void rangiereCzuA(){
+        gleisA.push(gleisC.top()); 
+        gleisC.pop();
+    }
+    public void rangiereCzuB(){
+        gleisB.push(gleisC.top()); 
+        gleisC.pop();
+    }
+
+}
---
+++ Klausurvorbereitung/Wagon.java
@@ -0,0 +1,13 @@
+package Klausurvorbereitung;
+
+public class Wagon {
+    private int gewicht; 
+
+    public Wagon(int gewicht){
+        this.gewicht = gewicht;
+    }
+
+    public int getGewicht(){
+        return gewicht;
+    }
+}
