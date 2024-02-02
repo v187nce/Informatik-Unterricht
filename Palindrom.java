de5acbedbe8c8712a717e201218bb96de76f4182
v187nce <134378283+v187nce@users.noreply.github.com>
2024-02-02 07:41:12 +0000

    aaa

--- Palindrom.java
+++ Palindrom.java
@@ -12,8 +12,13 @@ public class Palindrom{
         }
     }
 
+    public static boolean palindromKurz(String wort){
+        // return (wort.length() <= 1) ? result : palindromKurz(wort.substring(1, wort.length()-1), result); 
+        return (wort.length() <= 1) ? true : (wort.charAt(0) == wort.charAt(wort.length()-1)) ? palindromKurz(wort.substring(1, wort.length()-1)) : false;
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println(palindromKurz("anna")); 
     }
 
 
